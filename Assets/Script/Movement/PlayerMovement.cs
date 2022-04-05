93d9008261f5de55b64b6a7e45cc2d25e3e404dc
CleGuren <conglewin@gmail.com>
2022-04-05 05:50:44 -0700

    Basic character movement (WASD) with simple running animation, cleaned up file structure of the project. Next objective is to create a more smooth character animation, and add more decorations + monsters onto the map

--- Assets/Script/Movement/PlayerMovement.cs
+++ Assets/Script/Movement/PlayerMovement.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float Movespeed = 5;
+    public float moveSpeed = 5;
     public Rigidbody2D RigidBodyComponent;
+
+    Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        Input.GetAxisRaw("Horizontal");
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement.Normalize();
+
+        Vector2 charScale = transform.localScale;
+
+        if (movement.x < 0) { 
+            charScale.x = -1;
+        }
+
+        if (movement.x > 0) {
+            charScale.x = 1;
+        }
+        
+        transform.localScale = charScale;
     }
 
     void FixedUpdate() {
-
+        RigidBodyComponent.MovePosition(RigidBodyComponent.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
