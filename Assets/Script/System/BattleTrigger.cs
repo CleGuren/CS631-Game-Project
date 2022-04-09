3cba4d8a4f443574b7c96eedc12f031abfbd1745
CleGuren <conglewin@gmail.com>
2022-04-08 22:16:02 -0700

    battle system development 1.01

--- Assets/Script/System/BattleTrigger.cs
+++ Assets/Script/System/BattleTrigger.cs
@@ -6,10 +6,13 @@ using UnityEngine.SceneManagement;
 public class BattleTrigger : MonoBehaviour
 {
     public string EntityTag;
+    public string SceneToLoad;
+    private VectorValue PlayerValueStorage;
 
     void OnTriggerEnter2D(Collider2D entity) {
         if (entity.tag == "Player") {
-            SceneManager.LoadScene("Battle Scene");
+            PlayerValueStorage.CurrentPosition = GameObject.Find("Player").transform.position;
+            SceneManager.LoadScene(SceneToLoad);
         }
     }
 }
