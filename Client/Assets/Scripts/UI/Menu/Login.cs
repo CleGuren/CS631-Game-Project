d56aa7560c96edbfbf8dfb99becdf806b8092970
Jace <38768407+hongjie003@users.noreply.github.com>
2022-04-15 21:19:15 -0700

    Fix Issue with Duplicate Callback in Registration & Login
    Both script set to remove callback on destroy to prevent duplicate key/callback.

--- Client/Assets/Scripts/UI/Menu/Login.cs
+++ Client/Assets/Scripts/UI/Menu/Login.cs
@@ -2,74 +2,82 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor;
 using UnityEngine.UI;
-    public class Login : MonoBehaviour
+public class Login : MonoBehaviour
+{
+    // button login
+    public InputField UsernameInput;
+    public InputField PasswordInput;
+    public Button LoginButton;
+
+    private GameObject mainObject;
+    private string user_id = "";
+    private string password = "";
+    private MessageQueue msgQueue;
+    private ConnectionManager cManager;
+
+    void Awake()
     {
-        // button login
-        public InputField UsernameInput;
-        public InputField PasswordInput;
-        public Button LoginButton;
-        
-        private GameObject mainObject;
-        private string user_id = "";
-        private string password = "";
-        private MessageQueue msgQueue;
-        private ConnectionManager cManager;
-        
-        void Awake() {
-            mainObject = GameObject.Find("Network Manager");
-            cManager = mainObject.GetComponent<ConnectionManager>();
-            msgQueue = mainObject.GetComponent<MessageQueue> ();
-            msgQueue.AddCallback(Constants.SMSG_LOGIN, ResponseLogin);
-        }
+        mainObject = GameObject.Find("Network Manager");
+        cManager = mainObject.GetComponent<ConnectionManager>();
+        msgQueue = mainObject.GetComponent<MessageQueue>();
+        msgQueue.AddCallback(Constants.SMSG_LOGIN, ResponseLogin);
+    }
 
-        // Use this for initialization
-        void Start() {
-            LoginButton.onClick.AddListener(()=>
-            {
-                user_id = UsernameInput.text;
-                password = PasswordInput.text;
-                LoginButtonClick();
-            });
-        }
-        
-        public void LoginButtonClick()
+    // Use this for initialization
+    void Start()
+    {
+        LoginButton.onClick.AddListener(() =>
         {
-            user_id = user_id.Trim();
-            password = password.Trim();
-            if (user_id.Length == 0)
-            {
-                Debug.Log("User ID Required");
-            }else if (password.Length == 0)
-            {
-                Debug.Log("Password Required");
-            }
-            else
-            {
-                cManager.send(requestLogin(user_id, password));
-            }
-        }
+            user_id = UsernameInput.text;
+            password = PasswordInput.text;
+            LoginButtonClick();
+        });
+    }
 
-        public RequestLogin requestLogin(string username, string password)
+    private void OnDestroy()
+    {
+        msgQueue.RemoveCallback(Constants.SMSG_LOGIN);
+    }
+
+    public void LoginButtonClick()
+    {
+        user_id = user_id.Trim();
+        password = password.Trim();
+        if (user_id.Length == 0)
         {
-            RequestLogin request = new RequestLogin();
-            request.send(username, password);
-            print(username + ", " + password);
-            return request;
+            Debug.Log("User ID Required");
         }
-        
-        public void ResponseLogin(ExtendedEventArgs eventArgs)
+        else if (password.Length == 0)
         {
-            ResponseLoginEventArgs args = eventArgs as ResponseLoginEventArgs;
-            if (args.status == 0)
-            {
-                Debug.Log ("Successful Login response : " + args);
-                EditorUtility.DisplayDialog ("Login Successful", "You have successfully logged in." +
-                                                                 "\nClick Ok to continue execution and see responses on console", "Ok");
-                SceneManager.LoadScene("LoadingScene");
-            }
-            else
-            {
-                Debug.Log("Login Failed");
-            }
+            Debug.Log("Password Required");
+        }
+        else
+        {
+            cManager.send(requestLogin(user_id, password));
         }
     }
+
+    public RequestLogin requestLogin(string username, string password)
+    {
+        RequestLogin request = new RequestLogin();
+        request.send(username, password);
+        print(username + ", " + password);
+        return request;
+    }
+
+    public void ResponseLogin(ExtendedEventArgs eventArgs)
+    {
+        ResponseLoginEventArgs args = eventArgs as ResponseLoginEventArgs;
+        if (args.status == 0)
+        {
+            Debug.Log("Successful Login response : " + args);
+            EditorUtility.DisplayDialog("Login Successful", "You have successfully logged in." +
+                                                             "\nClick Ok to continue execution and see responses on console", "Ok");
+            SceneManager.LoadScene("LoadingScene");
+        }
+        else
+        {
+            Debug.Log("Login Failed");
+        }
+    }
+}
--- Client/Assets/Scripts/UI/Menu/Registration.cs
+++ Client/Assets/Scripts/UI/Menu/Registration.cs
@@ -2,77 +2,84 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor;
 using UnityEngine.UI;
-    public class Registration : MonoBehaviour
+public class Registration : MonoBehaviour
+{
+    // button login
+    public InputField UsernameInput;
+    public InputField PasswordInput;
+    public Button RegisterButton;
+
+    private GameObject mainObject;
+    private string user_id = "";
+    private string password = "";
+    private MessageQueue msgQueue;
+    private ConnectionManager cManager;
+
+    void Awake()
     {
-        // button login
-        public InputField UsernameInput;
-        public InputField PasswordInput;
-        public Button RegisterButton;
-        
-        private GameObject mainObject;
-        private string user_id = "";
-        private string password = "";
-        private MessageQueue msgQueue;
-        private ConnectionManager cManager;
-        
-        void Awake() {
-            mainObject = GameObject.Find("Network Manager");
-            cManager = mainObject.GetComponent<ConnectionManager>();
-            msgQueue = mainObject.GetComponent<MessageQueue> ();
-            msgQueue.AddCallback(Constants.SMSG_REGISTER, ResponseRegistration);
-        }
+        mainObject = GameObject.Find("Network Manager");
+        cManager = mainObject.GetComponent<ConnectionManager>();
+        msgQueue = mainObject.GetComponent<MessageQueue>();
+        msgQueue.AddCallback(Constants.SMSG_REGISTER, ResponseRegistration);
+    }
 
-        // Use this for initialization
-        void Start() {
-            RegisterButton.onClick.AddListener(()=>
-            {
-                user_id = UsernameInput.text;
-                password = PasswordInput.text;
-                RegistrationButtonClick();
-            });
-        }
+    // Use this for initialization
+    void Start()
+    {
+        RegisterButton.onClick.AddListener(() =>
+        {
+            user_id = UsernameInput.text;
+            password = PasswordInput.text;
+            RegistrationButtonClick();
+        });
+    }
 
-        public void RegistrationButtonClick()
-        {
-            user_id = user_id.Trim();
-            password = password.Trim();
-            if (user_id.Length == 0)
-            {
-                Debug.Log("User ID Required");
-            }else if (password.Length == 0)
-            {
-                Debug.Log("Password Required");
-            }
-            else
-            {
-                cManager.send(requestRegistration(user_id, password));
-            }
-        }
+    private void OnDestroy()
+    {
+        msgQueue.RemoveCallback(Constants.SMSG_REGISTER);
+    }
 
-        public RequestRegistration requestRegistration(string username, string password)
+    public void RegistrationButtonClick()
+    {
+        user_id = user_id.Trim();
+        password = password.Trim();
+        if (user_id.Length == 0)
         {
-            RequestRegistration request = new RequestRegistration();
-            request.send(username, password);
-            return request;
+            Debug.Log("User ID Required");
         }
-        
-        public void ResponseRegistration(ExtendedEventArgs eventArgs)
+        else if (password.Length == 0)
         {
-            ResponseRegistrationEventArgs args = eventArgs as ResponseRegistrationEventArgs;
-            if (args.status == 1) // status needs to be changed to reflect that the user cannot be created
-                                  // 1 for fail, 0 for success
-            {
-                Debug.Log ("Unsuccessful Registration response : " + args);
-                EditorUtility.DisplayDialog ("Registration unsuccessful", "Sorry, please try another username." +
-                                                                 "\nClick Ok to continue execution and see responses on console", "Ok");
-            }
-            else
-            {
-                // send the request to the backend for creation of a new user and password
-                // TODO 
-                Debug.Log("Registration Successful");
-                SceneManager.LoadScene("LoginScene");
-            }
+            Debug.Log("Password Required");
         }
+        else
+        {
+            cManager.send(requestRegistration(user_id, password));
         }
-    
+    }
+
+    public RequestRegistration requestRegistration(string username, string password)
+    {
+        RequestRegistration request = new RequestRegistration();
+        request.send(username, password);
+        return request;
+    }
+
+    public void ResponseRegistration(ExtendedEventArgs eventArgs)
+    {
+        ResponseRegistrationEventArgs args = eventArgs as ResponseRegistrationEventArgs;
+        if (args.status == 1) // status needs to be changed to reflect that the user cannot be created
+                              // 1 for fail, 0 for success
+        {
+            Debug.Log("Unsuccessful Registration response : " + args);
+            EditorUtility.DisplayDialog("Registration unsuccessful", "Sorry, please try another username." +
+                                                             "\nClick Ok to continue execution and see responses on console", "Ok");
+        }
+        else
+        {
+            // send the request to the backend for creation of a new user and password
+            // TODO 
+            Debug.Log("Registration Successful");
+            SceneManager.LoadScene("LoginScene");
+        }
+    }
+}
