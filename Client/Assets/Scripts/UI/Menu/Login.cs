e31ae6700ebab1f7a7dd00fb5508fbd539f60109
Tori Bialorucki <vbialorucki@mail.sfsu.edu>
2022-04-19 20:25:32 -0700

    commented out for tutorial; added stuff for town hub
    This reverts commit 11bacdecf77510e0bf582c4b4dabde5dadd9f451.

--- Client/Assets/Scripts/UI/Menu/Login.cs
+++ Client/Assets/Scripts/UI/Menu/Login.cs
@@ -77,7 +77,6 @@ public class Login : MonoBehaviour
             //                                                  "\nClick Ok to continue execution and see responses on console", "Ok");
             SceneManager.LoadScene("Tutorial");
             // SceneManager.LoadScene("Battle Scene");
-            //SceneManager.LoadScene("Town Hub");
         }
         else
         {
