e3e8b9fd80db6ccbe7b3f93e4c04bdb640abcdce
Tori Bialorucki <vbialorucki@mail.sfsu.edu>
2022-04-21 13:33:31 -0700

    added script for town hub and test shop transition

---
+++ Client/Assets/Scripts/System/TownHubToTestShop1.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TownHubToTestShop1 : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D entity)
+    {
+        if (entity.CompareTag("Player"))
+        {
+            Debug.Log("Collision Detected");
+            SceneManager.LoadScene("Test Shop");
+        }
+    }
+}
