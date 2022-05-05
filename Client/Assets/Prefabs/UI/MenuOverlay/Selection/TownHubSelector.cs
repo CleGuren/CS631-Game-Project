ce530e3e6f34a33620bfc8ae170caaf6da0e4757
Tori Bialorucki <vbialorucki@mail.sfsu.edu>
2022-05-04 23:32:15 -0700

    add prefab for door; move test shop into town hub; kill myself

--- Client/Assets/Prefabs/UI/MenuOverlay/Selection/TownHubSelector.cs
+++ Client/Assets/Prefabs/UI/MenuOverlay/Selection/TownHubSelector.cs
@@ -7,15 +7,17 @@ using Event = AK.Wwise.Event;
 
 public class TownHubSelector : MonoBehaviour
 {
-    //[SerializeField] private AK.Wwise.State currentState;
-    [SerializeField] private AK.Wwise.Event stopEvent;
+    [SerializeField] private AK.Wwise.State currentState;
+   // [SerializeField] private AK.Wwise.Event stopEvent;
    // [SerializeField] private AK.Wwise.Event currentEvent;
     public void TownHubOnButtonClick()
     {
+        currentState.SetValue();
         DontDestroyOnLoad(gameObject);
         if (SceneManager.GetActiveScene().name != "Town Hub")
         {
             AkSoundEngine.StopAll();
+            currentState.SetValue();
             SceneManager.LoadScene("Town Hub");
         }
     }
--- Client/Assets/Prefabs/UI/MenuOverlay/Selection/TutorialSelector.cs
+++ Client/Assets/Prefabs/UI/MenuOverlay/Selection/TutorialSelector.cs
@@ -6,15 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class TutorialSelector : MonoBehaviour
 {
-    //[SerializeField] private AK.Wwise.State currentState;
-    [SerializeField] private AK.Wwise.Event stopEvent;
+    [SerializeField] private AK.Wwise.State currentState;
+    //[SerializeField] private AK.Wwise.Event stopEvent;
     //[SerializeField] private AK.Wwise.Event currentEvent;
     public void TutorialOnButtonClick()
     {
+        currentState.SetValue();
         DontDestroyOnLoad(gameObject);
         if (SceneManager.GetActiveScene().name != "Tutorial")
         {
+            
             AkSoundEngine.StopAll();
+            currentState.SetValue();
             SceneManager.LoadScene("Tutorial");
         }
     }
