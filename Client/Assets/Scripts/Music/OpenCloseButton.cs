ee3dac7c6d9ee3516c39c969f57ee3561eb2cf2d
Tori Bialorucki <vbialorucki@mail.sfsu.edu>
2022-05-04 23:39:41 -0700

    openclosebutton junk

--- Client/Assets/Scripts/Music/OpenCloseButton.cs
+++ Client/Assets/Scripts/Music/OpenCloseButton.cs
@@ -1,1 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCloseButton : MonoBehaviour
 {
+    public GameObject GameObject;
+    public bool active;
+
+    public void OpenAndCloseButton()
+    {
+        if (active == false)
+        {
+            gameObject.SetActive(true);
+            active = true;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            active = false;
+        }
+    }
+}
